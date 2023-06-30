d51bcefb0c62a6325efd26ab94c2f0b5b4d6bc5c
John Doe <giganigga@example.com>
2023-06-30 12:49:48 +0200

    Field

---
+++ src/controller/Field.java
@@ -0,0 +1,43 @@
+package src.controller;
+
+public class Field {
+    public static void main(String[] args)
+    {
+        int fieldWidth = 10;
+        int fieldHight = 10;
+        char[][] playground = new char[fieldHight][fieldWidth];
+        for (int i=0; i< fieldHight; i++)
+        {
+            for(int j = 0 ;i < fieldWidth; j++){
+                playground[i][j] = ' ';
+            }
+        }
+        for (int i = 0; i < fieldHight; i++) 
+        {
+            for (int j = 0; j < fieldWidth; j++) {
+                System.out.print(playground[i][j] + " ");
+            }
+                System.out.println();
+        }
+
+    };
+}
+
+
+
+
+
+
+
+
+
+        
+/*        int[][] playground = new int[100][100];
+
+        for (int i = 0; i < playground.length; i++){
+            for (int j = 0; j < playground[i].length; j++)
+            {
+                System.out.print(playground[i][j]+ " ");
+            }
+            System.out.println();
+        }*/
