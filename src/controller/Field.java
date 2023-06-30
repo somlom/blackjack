ccbfedad0bc44166e97d3f6edb51aff4b6c57d37
John Doe <giganigga@example.com>
2023-06-30 13:38:48 +0200

    feld

--- src/controller/Field.java
+++ src/controller/Field.java
@@ -1,43 +1,63 @@
 package src.controller;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Field {
-    public static void main(String[] args)
-    {
-        int fieldWidth = 10;
-        int fieldHight = 10;
-        char[][] playground = new char[fieldHight][fieldWidth];
-        for (int i=0; i< fieldHight; i++)
-        {
-            for(int j = 0 ;i < fieldWidth; j++){
-                playground[i][j] = ' ';
-            }
-        }
-        for (int i = 0; i < fieldHight; i++) 
-        {
-            for (int j = 0; j < fieldWidth; j++) {
-                System.out.print(playground[i][j] + " ");
-            }
-                System.out.println();
+public class Field extends Frame implements KeyListener {
+    // Spielfeldgröße
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 600;
+
+    // Position und Größe der Schlange
+    private int snakeX;
+    private int snakeY;
+    private int snakeSize;
+
+    public Field() {
+        // Fenstereinstellungen
+        setSize(WIDTH, HEIGHT);
+        setTitle("Snake Game");
+        setVisible(true);
+        addKeyListener(this);
+
+        // Startposition und Größe der Schlange
+        snakeX = WIDTH / 2;
+        snakeY = HEIGHT / 2;
+        snakeSize = 1;
+    }
+
+    public void paint(Graphics g) {
+        // Hintergrund zeichnen
+        g.setColor(Color.white);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        // Schlange zeichnen
+        g.setColor(Color.GREEN);
+        g.fillRect(snakeX, snakeY, snakeSize, snakeSize);
+    }
+
+    public void keyPressed(KeyEvent e) {
+        // Tastatureingabe verarbeiten
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_LEFT) {
+            snakeX -= snakeSize;
+        } else if (keyCode == KeyEvent.VK_RIGHT) {
+            snakeX += snakeSize;
+        } else if (keyCode == KeyEvent.VK_UP) {
+            snakeY -= snakeSize;
+        } else if (keyCode == KeyEvent.VK_DOWN) {
+            snakeY += snakeSize;
         }
 
-    };
+        // Spielfeld neu zeichnen
+        repaint();
+    }
+
+    public void keyTyped(KeyEvent e) {}
+
+    public void keyReleased(KeyEvent e) {}
+
+    public static void main(String[] args) {
+        Field game = new Field();
+    }
 }
-
-
-
-
-
-
-
-
-
-        
-/*        int[][] playground = new int[100][100];
-
-        for (int i = 0; i < playground.length; i++){
-            for (int j = 0; j < playground[i].length; j++)
-            {
-                System.out.print(playground[i][j]+ " ");
-            }
-            System.out.println();
-        }*/
